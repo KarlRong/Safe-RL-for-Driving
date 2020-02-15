d18329f1b944e4fb1df11c00fc8ef79367fb6a04
rongjk <rongjk@xiaopeng.com>
2020-02-16 01:49:55 +0800

    change map sucess

---
+++ rl/train.py
@@ -0,0 +1,160 @@
+"""Runner script for single and multi-agent reinforcement learning experiments.
+
+This script performs an RL experiment using the PPO algorithm. Choice of
+hyperparameters can be seen and adjusted from the code below.
+
+Usage
+    python train.py EXP_CONFIG
+"""
+
+import argparse
+import json
+import os
+import sys
+from time import strftime
+
+import ray
+from ray import tune
+from ray.tune import run_experiments
+from ray.tune.registry import register_env
+from flow.utils.registry import make_create_env
+try:
+    from ray.rllib.agents.agent import get_agent_class
+except ImportError:
+    from ray.rllib.agents.registry import get_agent_class
+from copy import deepcopy
+
+from flow.core.util import ensure_dir
+from flow.utils.registry import env_constructor
+from flow.utils.rllib import FlowParamsEncoder, get_flow_params
+
+
+def parse_args(args):
+    """Parse training options user can specify in command line.
+
+    Returns
+    -------
+    argparse.Namespace
+        the output parser object
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Parse argument used when running a Flow simulation.",
+        epilog="python train.py EXP_CONFIG")
+
+    # required input parameters
+    parser.add_argument(
+        'exp_config', type=str,
+        help='Name of the experiment configuration file, as located in '
+             'exp_configs/rl/singleagent or exp_configs/rl/multiagent.')
+
+    # optional input parameters
+    parser.add_argument(
+        '--rl_trainer', type=str, default="RLlib",
+        help='the RL trainer to use. either RLlib or Stable-Baselines')
+
+    parser.add_argument(
+        '--num_cpus', type=int, default=1,
+        help='How many CPUs to use')
+    parser.add_argument(
+        '--num_steps', type=int, default=5000,
+        help='How many total steps to perform learning over')
+    parser.add_argument(
+        '--rollout_size', type=int, default=1000,
+        help='How many steps are in a training batch.')
+
+    return parser.parse_known_args(args)[0]
+
+def setup_exps_rllib(flow_params,
+                     n_cpus,
+                     n_rollouts):
+    """Return the relevant components of an RLlib experiment.
+
+    Parameters
+    ----------
+    flow_params : dict
+        flow-specific parameters (see flow/utils/registry.py)
+    n_cpus : int
+        number of CPUs to run the experiment over
+    n_rollouts : int
+        number of rollouts per training iteration
+
+    Returns
+    -------
+    str
+        name of the training algorithm
+    str
+        name of the gym environment to be trained
+    dict
+        training configuration parameters
+    """
+    horizon = flow_params['env'].horizon
+
+    alg_run = "PPO"
+
+    agent_cls = get_agent_class(alg_run)
+    config = deepcopy(agent_cls._default_config)
+
+    config["num_workers"] = n_cpus
+    config["num_cpus_per_worker"] = 1
+    config["use_pytorch"] = True
+    config["num_gpus"] = 0
+    config["train_batch_size"] = horizon * n_rollouts
+    config["gamma"] = 0.999  # discount rate
+    # config["model"].update({"fcnet_hiddens": [32, 32, 32]})
+    config["use_gae"] = True
+    config["lambda"] = 0.97
+    config["kl_target"] = 0.02
+    config["num_sgd_iter"] = 10
+    config['clip_actions'] = False  # FIXME(ev) temporary ray bug
+    config["horizon"] = horizon
+
+    # save the flow params for replay
+    flow_json = json.dumps(
+        flow_params, cls=FlowParamsEncoder, sort_keys=True, indent=4)
+    config['env_config']['flow_params'] = flow_json
+    config['env_config']['run'] = alg_run
+
+    create_env, gym_name = make_create_env(params=flow_params)
+
+    # Register as rllib env
+    register_env(gym_name, create_env)
+    return alg_run, gym_name, config
+
+
+if __name__ == "__main__":
+    flags = parse_args(sys.argv[1:])
+
+    # import relevant information from the exp_config script
+    module = __import__("rl.singleagent", fromlist=[flags.exp_config])
+    if hasattr(module, flags.exp_config):
+        submodule = getattr(module, flags.exp_config)
+    else:
+        assert False, "Unable to find experiment config!"
+    if flags.rl_trainer == "RLlib":
+        flow_params = submodule.flow_params
+        n_cpus = submodule.N_CPUS
+        # n_cpus = 11
+        n_rollouts = submodule.N_ROLLOUTS
+
+        alg_run, gym_name, config = setup_exps_rllib(
+            flow_params, n_cpus, n_rollouts)
+
+        ray.init(num_cpus=n_cpus + 1)
+        trials = run_experiments({
+            flow_params["exp_tag"]: {
+                "run": alg_run,
+                "env": gym_name,
+                "config": {
+                    **config
+                },
+                "checkpoint_freq": 1,
+                "checkpoint_at_end": True,
+                "max_failures": 999,
+                "stop": {
+                    "training_iteration": 200,
+                },
+            }
+        })
+    else:
+        assert False, "rl_trainer should be either 'RLlib'"
