040aefec38d8b4b71467aa08f00ab6a3e4854497
rongjk <rongjk@xiaopeng.com>
2020-05-03 18:05:20 +0800

    flow完成

--- traci_pedestrian_crossing/flow_ped.py
+++ traci_pedestrian_crossing/flow_ped.py
@@ -8,7 +8,9 @@ from flow.core.experiment import Experiment
 from flow.networks import Network
 
 # all other imports are standard
-from flow.core.params import VehicleParams
+from flow.core.params import VehicleParams, SumoCarFollowingParams, SumoLaneChangeParams
+from flow.controllers import IDMController
+from flow.core.params import InFlows
 from flow.core.params import NetParams
 from flow.core.params import InitialConfig
 from flow.core.params import EnvParams
@@ -17,7 +19,21 @@ from flow.core.params import EnvParams
 env_params = EnvParams()
 initial_config = InitialConfig()
 vehicles = VehicleParams()
-vehicles.add('human', num_vehicles=1)
+vehicles.add(
+    veh_id="human",
+    acceleration_controller=(IDMController, {
+        "noise": 0.2
+    }),
+    # lane_change_controller=(StaticLaneChanger, {}),
+    car_following_params=SumoCarFollowingParams(
+        speed_mode="obey_safe_speed",
+    ),
+    lane_change_params=SumoLaneChangeParams(
+        lane_change_mode=1621,
+        model="SL2015",
+        lc_impatience="0.1",
+        lc_time_to_impatience="1.0"
+    ))
 
 from flow.core.params import SumoParams
 
@@ -25,14 +41,51 @@ sim_params = SumoParams(render=True, sim_step=1)
 
 import os
 
+inflow = InFlows()
+inflow.add(veh_type="human",
+           edge="WC",
+           # depart_lane="best",
+           depart_lane=1,
+           arrivalLane=0,
+           probability=0.1,
+           depart_speed="random",
+           )
+inflow.add(veh_type="human",
+           edge="WC",
+           # depart_lane="best",
+           depart_lane=0,
+           arrivalLane=1,
+           probability=0.1,
+           depart_speed="random",
+           )
+inflow.add(veh_type="human",
+           edge="EC",
+           # depart_lane="best",
+           # vehs_per_hour=2000,
+           depart_lane=1,
+           arrivalLane=0,
+           probability=0.1,
+           depart_speed="random",
+           )
+inflow.add(veh_type="human",
+           edge="EC",
+           # depart_lane="best",
+           # vehs_per_hour=2000,
+           depart_lane=0,
+           arrivalLane=1,
+           probability=0.1,
+           depart_speed="random",
+           )
+
 net_params = NetParams(
     template={
     "net":"/home/rong/Safe-RL-for-Driving/traci_pedestrian_crossing/pedcrossing.net.xml",
     # features associated with the routes vehicles take
     "vtype": "/home/rong/Safe-RL-for-Driving/traci_pedestrian_crossing/pedcrossing.add.xml",
 
-    "rou":"/home/rong/Safe-RL-for-Driving/traci_pedestrian_crossing/data/pedcrossing.rou.xml",
+    # "rou":"/home/rong/Safe-RL-for-Driving/traci_pedestrian_crossing/data/pedcrossing.rou.xml",
     },
+    inflows=inflow,
 )
 
 # specify the edges vehicles can originate on
@@ -42,16 +95,17 @@ initial_config = InitialConfig(
 
 
 # specify the routes for vehicles in the network
-class TemplateNetwork(Network):
+class PedCrossing(Network):
 
     def specify_routes(self, net_params):
-        return {"-32410#3": ["-32410#3"]}
+        return {'EC': ['EC', 'CW'],
+                'WC': ['WC', 'CE']}
 
 if __name__ == "__main__":
     flow_params = dict(
         exp_tag='template',
         env_name=TestEnv,
-        network=TemplateNetwork,
+        network=PedCrossing,
         simulator='traci',
         sim=sim_params,
         env=env_params,
